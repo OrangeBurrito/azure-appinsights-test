ac53c1d07b1b48502b2378625c6fd84d48cce391
Paul Krisadaolarn <pauly.k@gmail.com>
2025-02-13 13:34:50 +0700

    add endpoints & app insights

--- Program.cs
+++ Program.cs
@@ -3,15 +3,24 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
 
-
+builder.Services.AddApplicationInsightsTelemetry();
+builder.Logging.AddApplicationInsights();
 var app = builder.Build();
 
 // Define a minimal API endpoint. The lambda receives an ILogger from DI.
-app.MapGet("/", (ILogger<Program> logger) =>
-{
+app.MapGet("/", (ILogger<Program> logger) => {
+    var yyyyMMdd_HHmmss = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
     // Log an information-level message
-    logger.LogInformation("Endpoint '/' was hit - returning hello world");
-    return "hello world";
+    logger.LogInformation("{time} - Endpoint '/' was hit - returning hello world", yyyyMMdd_HHmmss);
+    return $"hello world {yyyyMMdd_HHmmss}";
+});
+
+// get random number endpoint return number between 1000 and 9999
+app.MapGet("/random", (ILogger<Program> logger) => {
+    var random = new Random();
+    var randomNumber = random.Next(1000, 9999);
+    logger.LogInformation("{time} - Endpoint '/random' was hit - returning random number {randomNumber}", DateTime.Now.ToString("HH:mm:ss"), randomNumber);
+    return randomNumber;
 });
 
 app.Run();
