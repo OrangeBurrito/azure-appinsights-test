81492c3fc2aab4a65a73dd19464d27458ad4b7fe
Paul Krisadaolarn <pauly.k@gmail.com>
2025-02-13 12:56:19 +0700

    AddConsole

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// builder.Logging.ClearProviders();
-// builder.Logging.AddConsole();
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
 
 
 var app = builder.Build();
